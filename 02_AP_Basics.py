f691c20844f3c70ae5983fa12a45651d5415b80a
Jeslezi <jesse.lin@kingsway.school.nz>
2026-05-20 10:57:26 +1200

    Second code for learning perimeter and area

---
+++ 02_AP_Basics.py
@@ -0,0 +1,18 @@
+#Code to learn about variables
+
+#Variables can be used as string
+greeting = "Hello World"
+
+#Variables can be a number
+fav_num = 7
+
+#Variables can be boolean (True/False)
+love_programming = True
+
+print(greeting)
+print(fav_num)
+print(love_programming)
+
+#This variable has a problem
+my_name = "Jesse"
+print (my_name)
